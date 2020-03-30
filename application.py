045f140c92846f22e0f635d096861ae548e9640b
best303 <s246762@studenti.polito.it>
2020-03-30 14:19:05 +0200

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! This is Marco Capusso's webapp"
+    return "<h1>Hello World! This is Marco Capusso's webapp</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
